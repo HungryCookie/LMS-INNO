9c779e9ac80d480fa69d8cdfe242138ebe6a1adf
HungryCookie <and.olega@gmail.com>
2018-01-30 15:18:54 +0300

    Empty classes

--- src/main/java/Users.java
+++ src/main/java/Users.java
@@ -1,5 +1,47 @@
 public abstract class Users {
     private String name;
     private String password;
-    private int ID;
+    private int ID; //Card number of each user
+    private String address;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
