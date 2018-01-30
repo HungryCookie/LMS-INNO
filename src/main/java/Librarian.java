2e0e349fe7e10e15d8fe3d32165723d5d03d217a
HungryCookie <and.olega@gmail.com>
2018-01-30 16:48:28 +0300

    Interface

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -1,3 +1,21 @@
 public class Librarian extends Users {
+    public void addUser(String name, String address, String phoneNumber) {
+        /*Add user to database
+        * Create password
+        * Return Login and Password for new User*/
 
+        return;
+    }
+
+    public void changeName(String name) {
+        /*Change user's name with new one*/
+    }
+
+    public void changeNumber() {
+        /*Change user's phone number*/
+    }
+
+    public void changeAddress() {
+        /*Change user's address*/
+    }
 }
--- src/main/java/Patron.java
+++ src/main/java/Patron.java
@@ -1,3 +1,21 @@
 public class Patron extends Users{
+    private int fine;
 
+    public Patron() {
+        /*Renew Fine for current user*/
+    }
+
+    public void bookOrder(int bookID) {
+        /* Note that you ordered a book in database
+        * */
+    }
+
+    public void bookOrder(String name) {
+        /*Find bookID by name
+        * Use method bookOrder(ID) to order a book by ID*/
+    }
+
+    public int checkFine() {
+        return fine;
+    }
 }
