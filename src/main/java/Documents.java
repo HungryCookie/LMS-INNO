a4c30d04e1e8402ca69dc9b508cdc9481e5db7f2
Oleg <HungryCookie@users.noreply.github.com>
2018-01-31 22:36:34 +0300

    Update Documents.java

--- src/main/java/Documents.java
+++ src/main/java/Documents.java
@@ -1,24 +1,74 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
-
-public abstract class Documents {
+ 
+public class Documents {
     private String name;
     private String type; // Book, JA or AV material
     private int docID; //ID of proper document
     private String author;
     private int copies;
-
-    public static Documents(int docID){
-        
-        //if docID is not correct return null;
-        
-        //else get result by docID and set all the fields
+ 
+    private FcukBase base = new FcukBase();
+ 
+    public Documents(int docID){
+        ResultSet res = base.getBookByID(docID);
+ 
+        try {
+            //if docID is not correct return null;
+            if (!res.next()) {
+                return;
+            }
+            //else get result by docID and set all the field
+            this.docID = docID;
+ 
+            name = res.getString("name");
+            author = res.getString("author");
+            copies = res.getInt("counter");
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
-    
-    public static Documets(String name){
-        
-        //if name is not correct return null;
-        
-        //else get result by name and set all the fields
+ 
+    public Documents(String name){
+        ResultSet res = base.getBookByName(name);
+ 
+        try {
+            //if name is not correct return null;
+            if (!res.next()) {
+                return;
+            }
+            //else get result by name and set all the fields
+            this.docID = docID;
+ 
+            name = res.getString("name");
+            author = res.getString("author");
+            copies = res.getInt("counter");
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+ 
     }
-    
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public String getType() {
+        return type;
+    }
+ 
+    public int getDocID() {
+        return docID;
+    }
+ 
+    public String getAuthor() {
+        return author;
+    }
+ 
+    public int getCopies() {
+        return copies;
+    }
 }
