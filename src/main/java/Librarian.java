2cf8f53d3b1935674193875aff91a9fb49213d30
aahmetshin199 <aahmetshin199@gmail.com>
2018-02-04 18:18:41 +0300

    Librarian for Oleg

--- src/main/java/Librarian.java
+++ src/main/java/Librarian.java
@@ -1,4 +1,33 @@
 public class Librarian extends Users {
+    private FcukBase base = new FcukBase();
+    
+    public Librarian(int userID){
+
+        ResultSet res = base.getUserByID(userID);
+        try {
+            // if usedID is not correct then return null
+            /*if (!res.next()) {
+                return;
+            }*/
+            this.userID = userID;
+            // else get result by userID and set all the fields
+            name = res.getString("name");
+            address = res.getString("address");
+            password = res.getString("password");
+            phoneNumber = res.getString("phoneNumber");
+ 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Patron getPatronInfo(int userID){
+        
+        Patron p = new Patron(userID);
+        
+        return p;
+    }
+    
     public void addUser(String name, String address, String phoneNumber) {
         /*Add user to database
         * Create password
