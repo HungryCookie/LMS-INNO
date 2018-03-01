21827b1cd5ba1d0e75ece524b6a0634bb4dd1d47
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-01 15:42:23 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,116 +1,23 @@
-/*
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
-    public static Scanner sc = new Scanner(System.in);
+    public static Stage window;
+    public static Scene login;
 
-    public static void main(String[] args) throws Exception {
-        startLogin();
+    public static void main(String[] args) throws Exception { launch(args); }
+
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        login = new Scene(root, 600, 550);
+        window.setTitle("LibrINNO: Management System");
+        window.setScene(login);
+        window.show();
     }
 
-    public static void patronInfo(Librarian current) {
-        System.out.println("Enter patron's ID");
-        Patron patron = current.getPatronInfo(Integer.parseInt(sc.next()));
-        if (current == null) {
-            System.out.println("There is no such an user, try again");
-        }
-        else {
-            System.out.println("Name: " + patron.name + "\n" + "Address: " + patron.address + "\n" + "Phone number: " + patron.phoneNumber);
-            System.out.println("List of orders: ");
-            Documents [] order = patron.bookedDocuments();
-            for (Documents i : order) {
-                System.out.println("    " + i.getName());
-            }
-        }
-        endOfProgram(Integer.toString(current.getID()), current.getPassword());
-    }
-    public static void startLogin() throws Exception {
-        System.out.println("Enter your ID: ");
-        String id = sc.next();
-        System.out.println("Enter password");
-        checkID(id, sc.next());
-    }
-
-    public static Users findUser (String id) throws Exception {
-        Users user = null;
-        FcukBase base = new FcukBase();
-        if (base.checkUserID(Integer.parseInt(id))) {
-            String userStatus = base.getUserByID(Integer.parseInt(id)).getString("status");
-            if (userStatus.equals("Librarian")) {
-                user = new Librarian(Integer.parseInt(id));
-            }
-            else {
-                user = new Patron(Integer.parseInt(id));
-            }
-        } else {
-            return null;
-        }
-        return user;
-    }
-
-    public static void checkID(String id, String password) throws Exception {
-        Users current = findUser(id);
-        if (current == null) {
-            System.out.println("There is no such an user, try again");
-            startLogin();
-        } else {
-            if (!current.getPassword().equals(password)) {
-                System.out.println("Wrong password, try again");
-                startLogin();
-            } else {
-                if (current instanceof Librarian) {
-                    patronInfo((Librarian) current);
-                    endOfProgram(Integer.toString(current.getID()), current.getPassword());
-                } else if (current instanceof Patron) {
-                    System.out.println("Enter '1' to search for a new document, enter '2' to see list of your orders, enter '0' to exit");
-                    char ans = sc.next().charAt(0);
-                    if (ans == '0') System.exit(0);
-                    else if (ans == '1') {
-                        System.out.println("Enter title or ID of document to search");
-
-                        Documents document;
-
-                        String order = sc.next();
-                        try {
-                            document = new Documents(Integer.parseInt(order));
-                        } catch (Exception e) {
-                            System.out.println("+++++++" + order);
-                            document = new Documents(order);
-                        }
-
-                        if (document == null) {
-                            System.out.println("Sorry, there is no such a document, would you like to search for another one?");
-                        } else {
-                            if (current.bookADocument(document)) {
-                                System.out.println("Your document is successfully booked");
-                            } else {
-                                System.out.println("You have already booked this document before, repeating is prohibited");
-                            }
-                        }
-                    } else if (ans == '2') {
-                        Documents[] order = ((Patron) current).bookedDocuments();
-                        for (Documents i : order) {
-                            System.out.println(i.getName());
-                        }
-                    }
-                    endOfProgram(Integer.toString(current.getID()), current.getPassword());
-                }
-            }
-        }
-    }
-    public static void endOfProgram(String id, String password) {
-        System.out.println("To continue session enter '1', to exit program enter '0'");
-        char ans = sc.next().charAt(0);
-        if (ans == '1') try {
-            checkID(id, password);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        else if (ans == '0') System.exit(0);
-        else {
-            System.out.println("Unavailable input");
-            endOfProgram(id, password);
-        }
-    }
-}*/
+}
