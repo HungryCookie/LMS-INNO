805e4b99b23f89415b608d47a5d571b231b63e43
HungryCookie <and.olega@gmail.com>
2018-03-02 10:45:05 +0300

    Minor updates

--- src/main/java/FcukBaseInterface.java
+++ src/main/java/FcukBaseInterface.java
@@ -6,19 +6,20 @@ public interface FcukBaseInterface {
     ResultSet getDocumentByID(int bookID); // Get all info about certain document through its ID
     ResultSet getDocumentByName(String name); // Get all info about certain document through its name
     ResultSet getDocumentByAuthor(String author); // Get all info about certain document through its author
+    ResultSet checkedOutByUserID(int userID); // Checks all books checked out by certain user
+    ResultSet checkedOutByDocID(int bookID); // Checks all books checked out by its ID
     int addNewUser(String name, String phoneNumber, String address, String status, String password); // Adds new User to the database
     int addNewDocument(String name, String author, int counter, int cost, String reference, String bestseller); // Adds new document to the data base
     void userModify(int id, String name, String phoneNumber, String address, String status, String password); // Modifies user
     void documentModify(int id, String name, String author, int counter, int cost, String reference, String bestseller); // Modifies user
     void checkOut(int userID, int copyID, String date); // Checks the document out to the certein user
     void returnDoc(int copyID); // Returning the document to the library
-    boolean bookADocument(int docID, int userID); // Place an order on a certain document from certain user
+    void deleteUser(int userID); // Delete a certain user
     ArrayList findBookedDocuments(int userID); // Find all booked documents of user
     ArrayList findUserByBookedDocument(int docID); // find users who booked certain document
     int[] findCopyID(int docID); // Find ID of copies of books
     boolean checkUserID(int userID); // Check whether there exist such a user
     boolean checkDocumentByName(String name); // Check whether there exist such a doc
     boolean checkDocumentByID(int bookID); // Checks whether there is such a book by its ID
-    ResultSet checkedOutByUserID(int userID); // Checks all books checked out by certain user
-    ResultSet checkedOutByDocID(int bookID); // Checks all books checked out by its ID
+    boolean bookADocument(int docID, int userID); // Place an order on a certain document from certain user
 }
