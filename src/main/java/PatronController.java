e1a017ff6a1efd72f75746a0c2cece20c74154bf
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-03 19:40:55 +0300

    Update PatronController.java

--- src/main/java/PatronController.java
+++ src/main/java/PatronController.java
@@ -1,28 +1,26 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
 
 public class PatronController {
 
-    public TextField searchtext;
-    public Button search;
-    public Label note;
-    public Button back;
-    public TableView<Documents> table;
-    public TableColumn<Documents, String> title;
-    public TableColumn<Documents, String> author;
-    public TableColumn<Documents, String> type;
+    @FXML
+    private TextField searchtext;
+    @FXML
+    private Button search;
+    @FXML
+    private Label note;
+    @FXML
+    private Button back;
+    @FXML
     public Scene tableScene;
 
-    public void search() {
+    @FXML
+    private void search() {
         Documents document;
         String order = searchtext.getText();
         try {
@@ -46,25 +44,15 @@ public class PatronController {
         }
     }
 
-    public void back() throws Exception {
+    @FXML
+    private void back() throws Exception {
         Main.window.setScene(Main.login);
     }
 
-    public void info() throws Exception{
-        Stage win = new Stage();
-
-        ObservableList<Documents> docs = FXCollections.observableArrayList();
-        Documents[] order = ((Patron) Login.current).bookedDocuments();
-        for (Documents i : order) {
-            docs.add(i);
-        }
-        title.setCellValueFactory(new PropertyValueFactory<Documents, String>("name"));
-        author.setCellValueFactory(new PropertyValueFactory<Documents, String>("author"));
-        type.setCellValueFactory(new PropertyValueFactory<Documents, String>("type"));
-        table.setItems(docs);
+    @FXML
+    private void info() throws Exception{
         Parent tab = FXMLLoader.load(getClass().getResource("/table.fxml"));
         tableScene = new Scene(tab, 600, 550);
-        win.setScene(tableScene);
-        win.show();
+        Main.window.setScene(tableScene);
     }
 }
