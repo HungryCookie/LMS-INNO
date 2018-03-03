9ebb64b9b3eadc346352de3c826f2c92e6018082
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-03 19:40:10 +0300

    Update TableController.java

--- src/main/java/TableController.java
+++ src/main/java/TableController.java
@@ -1,22 +1,39 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 
 public class TableController {
 
-    public TableColumn title;
-    public TableColumn author;
-    public TableColumn type;
-    public TableColumn time;
-    public TableView<Documents> table;
+    @FXML
+    private TableView<Documents> table;
+    @FXML
+    private TableColumn<Documents, String> title;
+    @FXML
+    private TableColumn<Documents, String> author;
+    @FXML
+    private TableColumn<Documents, String> type;
+    @FXML
+    private Button back;
 
-    public void table() {
+    @FXML
+    private void initialize() {
         ObservableList<Documents> docs = FXCollections.observableArrayList();
         Documents[] order = ((Patron) Login.current).bookedDocuments();
         for (Documents i : order) {
             docs.add(i);
         }
+        title.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        author.setCellValueFactory(cellData -> cellData.getValue().authorProperty());
+        type.setCellValueFactory(cellData -> cellData.getValue().typeProperty());
+        table.setItems(docs);
+    }
+
+    @FXML
+    private void back() {
+        Main.window.setScene(Login.patronScene);
     }
 }
