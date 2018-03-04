5dff5ed14bc96b8956567bb383127858d9319220
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-04 21:31:34 +0300

    Update Copy.java

--- src/main/java/Copy.java
+++ src/main/java/Copy.java
@@ -4,27 +4,38 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Copy {
-    private IntegerProperty copyID;
+    private StringProperty copyID;
     private StringProperty name;
     private StringProperty type; // Book, JA or AV material
     private IntegerProperty docID; //ID of proper document
     private StringProperty author;
+    private StringProperty checkedOut;
 
     public Copy(String author, String name, int copyID) {
-        this.copyID = new SimpleIntegerProperty(copyID);
+        this.copyID = new SimpleStringProperty("" + copyID);
         this.author = new SimpleStringProperty(author);
         this.name = new SimpleStringProperty(name);
+    }
+
+    public Copy(int copyID, int userID) {
+        this.copyID = new SimpleStringProperty(""+copyID);
+        if (userID == 0) this.checkedOut = new SimpleStringProperty("No");
+        else this.checkedOut = new SimpleStringProperty("Yes");
     }
 
     public StringProperty authorProperty() {
         return author;
     }
 
-    public IntegerProperty copyIDProperty() {
+    public StringProperty copyIDProperty() {
         return copyID;
     }
 
     public StringProperty nameProperty() {
         return name;
+    }
+
+    public StringProperty checkedOutProperty() {
+        return checkedOut;
     }
 }
