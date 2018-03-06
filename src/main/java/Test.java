5171bc8db115592e6fd3b0795c5a4431aa96afe4
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-07 01:38:32 +0300

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,26 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Test {
 
-    public static void main(String[] args) {
+    private static Librarian lb = new Librarian(1);
+    private static Patron p1;
+    private static Patron p2;
+    private static Patron p3;
+    private static Documents b1;
+    private static Documents b2;
+    private static Documents b3;
+    private static Documents av1;
+    private static Documents av2;
+    private static Counter c = new Counter();
+
+    public static void main(String[] args) throws SQLException {
         start();
     }
 
-    public static void start() {
+    public static void start() throws SQLException {
         System.out.println("Enter number of test case (or '0' to exit): ");
         Scanner sc = new Scanner(System.in);
         String ans = sc.next();
@@ -20,214 +34,166 @@ public class Test {
             case 7: TC7();
             case 8: TC8();
             case 9: TC9();
-            case 10: TC10();
             default: System.exit(0);
         }
     }
 
-    public static void TC1() {
-        Patron ptr = new Patron(1);
-        Librarian lbr = new Librarian(2);
-        String name = "Discrete Math";
-        Documents doc = new Documents(name);
-        System.out.println("Starting test case 1. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+ptr.getName());
-        int week = ptr.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+    private static void initialize() {
+        int users = c.CountUsers();
+        int copies = c.CountCopies();
+        System.out.println("Number of users in system: " + users);
+        System.out.println("Number of documents in system: " + copies);
+    }
+
+    public static void TC1() throws SQLException {
+        System.out.println("Before action: ");
+        initialize();
+        p1 = new Patron(lb.addUser("Sergey Afonso", "30001", "Via Margutta, 3", "FacultyMember").intField);
+        p2 = new Patron(lb.addUser("Nadia Teixeira", "30002", "Via Sacra, 13", "Student").intField);
+        p3 = new Patron(lb.addUser("Elvira Espindola", "30003", "Via del Corso, 22", "Student").intField);
+        lb.addDocument("Introduction to Algorithms", "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivestand Clifford Stein", "MIT Press", "2009", 3, 0, "Third Edition", "book", "F", "F");
+        lb.addDocument("Design Patterns: Elements of Reusable Object-Oriented Software", "Erich Gamma, Ralph Johnson, John Vlissides, Richard Helm", "Addison-Wesley Professional", "2003", 2, 0, "First Edition", "book", "T", "F");
+        lb.addDocument("The Mythical Man-mont", "Brooks, Jr., Frederick P", "Addison-Wesley Longman Publishing Co.,Inc.", "1995", 1, 0, "Second edition", "book", "F", "T");
+        lb.addDocument("Null References: The Billion Dollar Mistake", "Tony Hoare", "", "", 0, 0, "", "AV", "F", "F");
+        lb.addDocument("Information Entropy", "Claude Shannon", "", "", 0, 0, "", "AV", "F", "F");
+        b1 = new Documents("Introduction to Algorithms");
+        b2 = new Documents("Design Patterns: Elements of Reusable Object-Oriented Software");
+        b3 = new Documents("The Mythical Man-mont");
+        av1 = new Documents("Null References: The Billion Dollar Mistake");
+        av2 = new Documents("Information Entropy");
+        System.out.println("After action: ");
+        initialize();
         start();
     }
 
-    public static void TC2() {
-        Patron ptr = new Patron(5);
-        Librarian lbr = new Librarian(2);
-        String name = "Impossible book";
-        Documents doc = new Documents(name); // id = 2
-        System.out.println("Starting test case 2. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+ptr.getName());
-        int week = ptr.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
+    public static void TC2() throws SQLException {
+        System.out.println("Before action: ");
+        initialize();
+        ResultSet rs = lb.copiesOfDocument(b1.getDocID());
+        while (rs.next()) {
+            int i = 2;
+            while (i > 0) {
+                if (rs.getInt("userID") == 0) {
+                    i--;
+                    lb.deleteCopy(rs.getInt("copyID"));
+                }
+            }
         }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+        rs = lb.copiesOfDocument(b2.getDocID());
+        while (rs.next()) {
+            int i = 1;
+            while (i > 0) {
+                if (rs.getInt("userID") == 0) {
+                    i--;
+                    lb.deleteCopy(rs.getInt("copyID"));
+                }
+            }
+        }
+        lb.deleteUser(p2.getID());
+        System.out.println("After action: ");
+        initialize();
         start();
     }
 
-    public static void TC3() {
-        Patron fclt = new Patron(3);
-        Patron std = new Patron(6);
-        Librarian lbr = new Librarian(2);
-        String name = "Discrete Math";
-        Documents doc = new Documents(name); // id = 2
-        System.out.println("Starting test case 3. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+fclt.getName());
-        int week = fclt.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+    public static void TC3() throws SQLException {
+        System.out.println("Before action: ");
+        initialize();
+        userInfo(p1);
+        System.out.println("----------------");
+        userInfo(p3);
+        System.out.println("After action: ");
+        initialize();
         start();
     }
 
-    public static void TC4() {
-        Patron fclt = new Patron(7);
-        Patron std = new Patron(8);
-        Librarian lbr = new Librarian(2);
-        String name = "Touch of Class";
-        Documents doc = new Documents(name); // id = 2
-        System.out.println("Starting test case 4. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+fclt.getName());
-        int week = fclt.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+    public static void TC4() throws SQLException {
+        System.out.println("Before action: ");
+        initialize();
+        userInfo(p2);
+        System.out.println("----------------");
+        userInfo(p3);
+        System.out.println("After action: ");
+        initialize();
         start();
     }
 
-    public static void TC5() {
-        Patron ptr1 = new Patron(9);
-        Patron ptr2 = new Patron(10);
-        Patron ptr3 = new Patron(11);
-        String name = "Academy";
-        Librarian lbr = new Librarian(2);
-        Documents doc = new Documents(name);
-        System.out.println("Starting test case 5. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+ptr1.getName()+", "+ptr2.getName()+" and "+ptr3.getName());
-        int week1 = ptr1.bookADocument(doc);
-        int week2 = ptr2.bookADocument(doc);
-        int week3 = ptr3.bookADocument(doc);
-        if (week1 != 0) {
-            System.out.println("The document is successfully booked for " + week1 + " weeks");
-        }
-        else System.out.println("It's impossible to check out this document");
-        if (week2 != 0) {
-            System.out.println("The document is successfully booked for " + week2 + " weeks");
-        }
-        else System.out.println("It's impossible to check out this document");
-        if (week3 != 0) {
-            System.out.println("The document is successfully booked for " + week3 + " weeks");
-        }
-        else System.out.println("It's impossible to check out this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: "+ doc.getCopies());
-        System.out.println("**********************");
+    public static void TC5() throws SQLException {
+        System.out.println("Before action: ");
+        initialize();
+        if (!lb.checkOut(p2.getID(), b1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        System.out.println("After action: ");
+        initialize();
         start();
     }
 
-    public static void TC6() {
-        System.out.println("Starting test case 6. ");
-        TC1();
-    }
-
-    public static void TC7() {
-        Patron ptr1 = new Patron(13);
-        Patron ptr2 = new Patron(16);
-        Librarian lbr = new Librarian(2);
-        String name = "Discrete Math";
-        Documents doc = new Documents(name); // id = 2
-        System.out.println("Starting test case 7. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+ptr1.getName()+" and "+ ptr2.getName());
-        int week = ptr1.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        week = ptr2.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+    public static void TC6() throws SQLException {
+        System.out.println("Before actions: ");
+        initialize();
+        p1.bookADocument(b1);
+        p3.bookADocument(b1);
+        p3.bookADocument(b2);
+        if (!lb.checkOut(p1.getID(), b1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p3.getID(), b1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p3.getID(), b2)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        userInfo(p1);
+        System.out.println("---------------");
+        userInfo(p3);
+        System.out.println("After actions: ");
+        initialize();
         start();
     }
 
-    public static void TC8() {
-        Patron fclt = new Patron(14);
-        Patron std = new Patron(17);
-        Librarian lbr = new Librarian(2);
-        String name = "Discrete Math";
-        Documents doc = new Documents(name);
-        System.out.println("Starting test case 8. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+std.getName());
-        int week = std.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
+    public static void TC7() throws SQLException {
+        System.out.println("Before actions: ");
+        initialize();
+        p1.bookADocument(b1);
+        p3.bookADocument(b1);
+        p1.bookADocument(b2);
+        p3.bookADocument(b2);
+        p1.bookADocument(b3);
+        p1.bookADocument(av1);
+        p3.bookADocument(av1);
+        if (!lb.checkOut(p1.getID(), b1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p1.getID(), b2)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p1.getID(), b3)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p1.getID(), av1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p3.getID(), b1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p3.getID(), b2)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        if (!lb.checkOut(p3.getID(), av1)) System.out.println("Impossible to check out");
+        else System.out.println("Checked out successfully");
+        userInfo(p1);
+        System.out.println("----------------");
+        userInfo(p3);
+        System.out.println("After actions: ");
+        initialize();
         start();
     }
 
-    public static void TC9() {
-        Patron fclt = new Patron(15);
-        Patron std = new Patron(18);
-        Librarian lbr = new Librarian(2);
-        String name = "Touch of Class";
-        Documents doc = new Documents(name);
-        System.out.println("Starting test case 9. ");
-        System.out.println("Number of copies in library: " + doc.getCopies());
-        System.out.println("Try to book "+doc.getName()+" for "+std.getName());
-        int week = std.bookADocument(doc);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
+    private static void userInfo(Users user) throws SQLException {
+        if (user == null) System.out.println("User is not a patron of library");
+        else {
+            System.out.println("Name: " + user.getName());
+            System.out.println("Address: " + user.getAddress());
+            System.out.println("Phone number: " + user.getPhoneNumber());
+            System.out.println("Status: " + user.getClass().getName());
+            System.out.println("ID: " + user.getID());
+            System.out.println("Checked out documents: ");
+            ResultSet rs = lb.checkedOut(user.getID());
+            while (rs.next()) {
+                System.out.println("         " + rs.getString("name") + " checked out at " + rs.getString("date"));
+            }
+            System.out.println();
         }
-        else System.out.println("It's impossible to book this document");
-        doc = new Documents(name);
-        System.out.println("Number of copies after action: " + doc.getCopies());
-        System.out.println("**********************");
-        start();
-    }
-
-    public static void TC10() {
-        Patron ptr = new Patron(25);
-        Librarian lbr = new Librarian(2);
-        Documents docNoRef = new Documents("Discrete Math");
-        Documents docRef = new Documents("Computer Architecture");
-        System.out.println("Number of copies (non-referal book): " + docNoRef.getCopies());
-        System.out.println("Number of copies (referal book): " + docRef.getCopies());
-        System.out.println("Try to book non-referal doc "+docNoRef.getName()+" for "+ptr.getName());
-        int week = ptr.bookADocument(docNoRef);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        System.out.println("Try to book referal doc "+docRef.getName()+" for "+ptr.getName());
-        week = ptr.bookADocument(docRef);
-        if (week != 0) {
-            System.out.println("The document is successfully booked for " + week + " weeks");
-        }
-        else System.out.println("It's impossible to book this document");
-        docNoRef = new Documents("Discrete Math");
-        System.out.println("Number of copies after action (non-referal): " + docNoRef.getCopies());
-        docRef = new Documents("Computer Architecture");
-        System.out.println("Number of copies after action (referal): " + docRef.getCopies());
-        start();
     }
 
 }
