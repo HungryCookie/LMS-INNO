b9e45226472ca71b46594914426ae99eaa61cde6
Dapimex <35925624+Dapimex@users.noreply.github.com>
2018-03-30 22:07:01 +0300

    Update LibrarianController.java

--- src/main/java/LibrarianController.java
+++ src/main/java/LibrarianController.java
@@ -1,13 +1,15 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LibrarianController {
 
@@ -17,8 +19,6 @@ public class LibrarianController {
     private Button addUser;
     @FXML
     private Button modifyUser;
-    @FXML
-    private Button checkOut;
     @FXML
     private Button bookings;
     @FXML
@@ -32,11 +32,40 @@ public class LibrarianController {
     @FXML
     private Button Copy;
     @FXML
-    public TextField userID;
+    public TextField searchUser;
     @FXML
-    private Label userError;
+    private TextField searchtext;
     @FXML
     private Label docError;
+    @FXML
+    private TableColumn<Users, String> names;
+    @FXML
+    private TableColumn<Users, String> ids;
+    @FXML
+    private TableView<Users> usrs;
+    @FXML
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label title;
+    @FXML
+    private Label author;
+    @FXML
+    private Label type;
+    @FXML
+    private Label edit;
+    @FXML
+    private Label publisher;
+    @FXML
+    private Label date;
+    @FXML
+    private Label bestseller;
+    @FXML
+    private Label status;
+    private FcukBase fb = new FcukBase();
 
     public static int userId;
 
@@ -51,6 +80,114 @@ public class LibrarianController {
     public static Stage dialog = new Stage();
     @FXML
     public Scene tableScene;
+    @FXML
+    private TableColumn<Documents, String> authorCol;
+    @FXML
+    private TableColumn<Documents, String> titleCol;
+    @FXML
+    private TableView<Documents> tab;
+
+    @FXML
+    private void initialize() throws SQLException {
+        title.setText("");
+        author.setText("");
+        publisher.setText("");
+        type.setText("");
+        edit.setText("");
+        date.setText("");
+        bestseller.setText("");
+        ObservableList<Documents> docs = FXCollections.observableArrayList();
+        ResultSet order = fb.getDocs();
+        while (order.next()) {
+            Documents copy = new Documents(order.getString("name"));
+            docs.add(copy);
+        }
+        titleCol.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        authorCol.setCellValueFactory(cellData -> cellData.getValue().authorProperty());
+        tab.setItems(docs);
+        tab.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> selectedDoc(newValue));
+        searchtext.textProperty().addListener(((observable, oldValue, newValue) -> {
+            try {
+                search(newValue);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }));
+        name.setText("");
+        address.setText("");
+        phone.setText("");
+        status.setText("");
+        ObservableList<Users> users = FXCollections.observableArrayList();
+        ResultSet orderU = fb.getUsers();
+        while (orderU.next()) {
+            Users usr;
+            if (orderU.getString("status").equals("Librarian"))
+                usr = new Librarian(orderU.getInt("id"));
+            else usr = new Patron(orderU.getInt("id"));
+            users.add(usr);
+        }
+        names.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
+        ids.setCellValueFactory(cellData -> cellData.getValue().getIDProperty().asString());
+        usrs.setItems(users);
+        usrs.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> selectedUser(newValue));
+        searchUser.textProperty().addListener(((observable, oldValue, newValue) -> {
+            try {
+                search(newValue);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+
+    @FXML
+    private void selectedDoc(Documents doc) {
+        title.setText(doc.getName());
+        author.setText("Author: " + doc.getAuthor());
+        type.setText("Type: " + doc.getType());
+        if (!doc.getType().equals("AV")) {
+            if (!publisher.getText().equals(""))publisher.setText("Publisher: " + doc.getPublisher());
+            if (!date.getText().isEmpty()) date.setText("Date: " + doc.getYear());
+            if (doc.isBestseller()) bestseller.setText("Bestseller");
+            else bestseller.setText("");
+            if (doc.getType().equals("Book")) {
+                if (!edit.getText().isEmpty()) edit.setText("Edition: " + doc.getEdition());
+            }
+            else {
+                if (!edit.getText().isEmpty()) edit.setText("Editor: " + doc.getEdition());
+            }
+        }
+        else {
+            publisher.setText("");
+            date.setText("");
+            bestseller.setText("");
+            edit.setText("");
+        }
+    }
+
+    @FXML
+    private void search(String queue) throws SQLException {
+        ObservableList<Users> users = FXCollections.observableArrayList();
+        ResultSet order = fb.getUsers();
+        while (order.next()) {
+            Users usr;
+            if (order.getString("status").equals("Librarian"))
+                usr = new Librarian(order.getInt("id"));
+            else usr = new Patron(order.getInt("id"));
+            if ((usr.getName().contains(queue)) || ((""+usr.getID()).contains(queue))) users.add(usr);
+        }
+        names.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
+        ids.setCellValueFactory(cellData -> cellData.getValue().getIDProperty().asString());
+        usrs.setItems(users);
+        usrs.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> selectedUser(newValue));
+    }
+
+    @FXML
+    private void selectedUser(Users usr) {
+        name.setText(usr.getName());
+        phone.setText(usr.getPhoneNumber());
+        address.setText(usr.getAddress());
+        status.setText(usr.getStatus());
+    }
 
     @FXML
     private void back() {
@@ -58,26 +195,13 @@ public class LibrarianController {
     }
 
     @FXML
-    private void checkOut() throws Exception{
-        try {
-        dialog.setTitle("Check out document");
-        userId = Integer.parseInt(userID.getText());
-        Parent root = FXMLLoader.load(getClass().getResource("/Checkout.fxml"));
-        dialog.setScene(new Scene(root, 315, 155));
-        dialog.show();
-    } catch (Exception e) {
-            userError.setText("Number is required");
-        }
-    }
-
-    @FXML
     private void returnDoc() throws IOException {
         try {
-            userId = Integer.parseInt(userID.getText());
+            userId = usrs.getSelectionModel().getSelectedItem().getID();
             Parent root = FXMLLoader.load(getClass().getResource("/Returndoc.fxml"));
             Main.window.setScene(new Scene(root, 600, 400));
         } catch (Exception e) {
-            userError.setText("Number is required");
+            name.setText("Choose a user...");
         }
     }
 
@@ -103,11 +227,11 @@ public class LibrarianController {
         try {
             action = "modified";
             object = "User";
-            userId = Integer.parseInt(userID.getText());
+            userId = usrs.getSelectionModel().getSelectedItem().getID();
             Parent root = FXMLLoader.load(getClass().getResource("/Userinfo.fxml"));
             Main.window.setScene(new Scene(root, 600, 400));
         }catch (Exception e) {
-            userError.setText("Number is required");
+            name.setText("Choose a user...");
         }
     }
 
@@ -116,11 +240,11 @@ public class LibrarianController {
         try {
             action = "modified";
             object = "Document";
-            docId = Integer.parseInt(docID.getText());
+            docId = tab.getSelectionModel().getSelectedItem().getDocID();
             Parent root = FXMLLoader.load(getClass().getResource("/Docinfo.fxml"));
             Main.window.setScene(new Scene(root, 600, 400));
         }catch (Exception e) {
-            docError.setText("Number is required");
+            title.setText("Choose a document...");
         }
     }
 
@@ -131,16 +255,18 @@ public class LibrarianController {
             object = "User";
             action = "deleted";
             docId = 0;
-            userId = Integer.parseInt(userID.getText());
+            userId = usrs.getSelectionModel().getSelectedItem().getID();
             if (((Librarian)Login.current).deleteUser(userId)) {
                 dialog = new Stage();
                 dialog.setTitle("User deleted");
                 Parent root = FXMLLoader.load(getClass().getResource("/Dialog.fxml"));
                 dialog.setScene(new Scene(root, 315, 155));
                 dialog.show();
+                int index = usrs.getSelectionModel().getSelectedIndex();
+                usrs.getItems().remove(index);
             }
         }catch (Exception e) {
-            userError.setText("Number is required");
+            name.setText("Choose a user...");
         }
     }
 
@@ -166,23 +292,23 @@ public class LibrarianController {
     @FXML
     private void Copy() {
         try {
-            docId = Integer.parseInt(docID.getText());
+            docId = tab.getSelectionModel().getSelectedItem().getDocID();
             Parent root = FXMLLoader.load(getClass().getResource("/CopiesActions.fxml"));
             Main.window.setScene(new Scene(root, 600, 400));
         }catch (Exception e) {
-            docError.setText("Number is required");
+            title.setText("Choose a document...");
         }
     }
 
     @FXML
     private void bookings() throws Exception {
-        try {
-            userId = Integer.parseInt(userID.getText());
-            Parent tab = FXMLLoader.load(getClass().getResource("/table.fxml"));
-            tableScene = new Scene(tab, 600, 550);
-            Main.window.setScene(tableScene);
-        }catch (Exception e) {
-            userError.setText("Number is required");
-        }
+//        try {
+//            userId = usrs.getSelectionModel().getSelectedItem().getID();
+//            Parent tab = FXMLLoader.load(getClass().getResource("/table.fxml"));
+//            tableScene = new Scene(tab, 600, 550);
+//            Main.window.setScene(tableScene);
+//        }catch (Exception e) {
+//            userError.setText("Number is required");
+//        }
     }
 }
