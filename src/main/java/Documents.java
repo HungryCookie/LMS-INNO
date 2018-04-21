3a89d7800fff19ac4b145c98424fd164313c1395
aahmetshin199 <aahmetshin199@gmail.com>
2018-04-21 22:02:49 +0300

    Update Documents.java

--- src/main/java/Documents.java
+++ src/main/java/Documents.java
@@ -129,7 +129,7 @@ public class Documents {
     }
 
     public boolean isReference() {
-        return (counter.get() <= 1 || type.get().equals("Magazine"));
+        return (counter.get() <= 1);
     }
 
 
